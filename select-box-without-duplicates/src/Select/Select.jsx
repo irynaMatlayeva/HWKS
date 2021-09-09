f7ecb584b28d9630902ee27dde53f68cb93ddc8d
irynaMatlayeva <iryna.matlayeva@gmail.com>
2021-09-09 12:33:54 +0300

    add select component with iterate over the collection once after receiving the response

--- select-box-without-duplicates/src/Select/Select.jsx
+++ select-box-without-duplicates/src/Select/Select.jsx
@@ -8,37 +8,51 @@ const {Option} = Select;
 const SelectSizesComponent = ({clusterList}) => {
   const [optionsList, setOptionsList] = useState({})
 
-  const uniqueSizesList = new Map(clusterList.map(item => [item['size'] && item['frame_size'],
-    {id: item.id, frameSize: item.frame_size, size: item.size}]))
-
   useEffect(() => {
-    const obj = {};
-    for (const [key, value] of uniqueSizesList) obj[key] = value;
+    function compositeKey (current) {
+      const {frameSize, size} = current
+      return frameSize ? `${ size } | ${ frameSize }` : size;
+    }
 
-    setOptionsList(prev => ({
-      ...prev,
-      ...obj
-    }))
+    function createObj (id, val) {
+      const {frameSize, size} = val
+      return {id, frameSize, size}
+    }
+
+    function updatedObject (acc, current) {
+      const prevState = acc[compositeKey(current)]
+      return createObj([...prevState.id, current.id], prevState)
+    }
+
+    function newObject (current) {
+      return createObj([current.id], current)
+    }
+
+    function getData (acc, current) {
+      return acc[compositeKey(current)] ? updatedObject(acc, current) : newObject(current);
+    }
+
+    const arrToObjects = clusterList.reduce((acc, current) => ({...acc, [compositeKey(current)]: getData(acc, current)}), {})
+    setOptionsList(arrToObjects)
   }, [clusterList])
 
   const handleChange = value => {
-    console.log(`selected id ${ value }`);
+    if (value) {
+      console.log(`selected id ${ optionsList[value].id }`);
+    }
   }
-
-  const renderOptions = Object.keys(optionsList).map((val) => {
-    const {id, size, frameSize} = optionsList[val]
-    return <Option key={ id } className='select__text' value={ handleChange() }>
-      { frameSize ? `${ size } | ${ frameSize }` : size }
-    </Option>
-  })
 
   return (
     <Form className='select' name='select-form'>
       <Title level={ 3 } className='select__title'>Size</Title>
       <Select className='select__content'
-              defaultValue={ handleChange() || 'Please select the size' }
+              defaultValue={ 'Please select the size' }
               onChange={ handleChange }>
-        { renderOptions }
+        { Object.keys(optionsList).map(value => {
+          return <Option key={ value } className='select__text' value={ value }>
+            { value }
+          </Option>
+        }) }
       </Select>
     </Form>
   );
--- select-box-without-duplicates/src/api/mock.js
+++ select-box-without-duplicates/src/api/mock.js
@@ -1,44 +1,53 @@
-export const cluster = {
+const camelcaseKeys = require('camelcase-keys');
+
+const cluster = {
   variations: [
     {
       id: 13035,
-      frame_size: "M",
+      frame_size: 'M',
       size: 175
     },
     {
       id: 13039,
-      frame_size: "L",
+      frame_size: 'L',
       size: 185
     },
     {
       id: 13040,
-      frame_size: "M",
+      frame_size: 'M',
       size: 185
     },
     {
       id: 13036,
-      frame_size: "M",
+      frame_size: 'M',
       size: 175
     },
     {
       id: 13037,
-      frame_size: "XXL",
+      frame_size: 'XXL',
       size: 195
     },
     {
       id: 13038,
-      frame_size: "XL",
+      frame_size: 'XL',
       size: 195
     },
     {
       id: 13041,
-      frame_size: "S",
+      frame_size: 'S',
       size: 165
     },
     {
       id: 13042,
-      frame_size: "S",
+      frame_size: 'S',
       size: 165
     }
   ]
+}
+
+camelcaseKeys({'foo-bar': true, nested: {unicorn_rainbow: true}},
+  {deep: true, pascalCase: true});
+
+export const getSizes = () => {
+  return camelcaseKeys(cluster.variations)
 }
